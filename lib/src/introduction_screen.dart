e73d69488e09ac7b508d926c49cc0bf1c32c4544
Koki Naniwa <namihna@icloud.com>
2020-12-21 14:52:00 +0900

    Fix size delete

--- lib/src/introduction_screen.dart
+++ lib/src/introduction_screen.dart
@@ -187,14 +187,14 @@ class IntroductionScreenState extends State<IntroductionScreen> {
     final captionsWidget = Container(
       child: widget.captions,
     );
-    final size = MediaQuery.of(context).size;
+    // final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: widget.globalBackgroundColor,
       body: Column(
         children: [
           SizedBox(
-            width: size.width,
-            height: size.height / 1.8,
+            // width: size.width,
+            // height: size.height / 1.8,
             child: NotificationListener<ScrollNotification>(
               onNotification: _onScroll,
               child: PageView(
